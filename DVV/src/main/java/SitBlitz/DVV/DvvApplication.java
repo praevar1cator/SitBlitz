b54aed48b00910e876f45c0032702de5825c4287
praevar1cator <kler_1234@mail.ru>
2024-02-23 14:43:49 +0300

    proj

---
+++ DVV/src/main/java/SitBlitz/DVV/DvvApplication.java
@@ -0,0 +1,13 @@
+package SitBlitz.DVV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvvApplication.class, args);
+	}
+
+}
